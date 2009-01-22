a774cb0b10b7b6904f8e463966f21a20167278ab
danilopopeye <danilopopeye@fdbd5f08-e324-11dd-a6ea-a32d6b26a33d>
2009-01-22 21:07:53 +0000

    Version 0.2; 	* Add/Get History 	* Add/Get/Update Task 	* Modal update 	* JS Modulation 	* PHP Functions 	* Add the release notes file;

--- trunk/includes/ajax.php
+++ trunk/includes/ajax.php
@@ -1,27 +1,107 @@
 <?php
 error_reporting( 0 );
-$error_array = error_get_last();
-
-require('function.php');
-
+require('functions.php');
 header('Content-type: text/json;');
 
+$conn = connect();
+
 switch( $_REQUEST['action'] ){
+	// HISTORY
 	case 'addHistory': addHistory(); break;
+	case 'getHistory': getHistory(); break;
+	// TASK
+	case 'addTask': addTask(); break;
+	case 'getTask': getTask(); break;
+	case 'getTaskByHistory': getTaskByHistory(); break;
+	case 'saveStatus': saveStatus(); break;
 	default: blank();
 }
+//	print_r( $_REQUEST );
+
+// HISTORY
 
 function addHistory(){
-	echo utf8_encode( '{ errro: 1, message: "addHistory..." }' );
+	$return = insert(
+		'histories', array( 'name','text' ), array( $_REQUEST['name'],$_REQUEST['text'] )
+	);
+
+	echo '{ code: ', $return['code'] ,', id: ', $return['id'] ,', message: "',(
+		$return['code'] ? 'error' : 'Ok!'
+	),'" }';
 }
+
+function getHistory(){
+	$return = getJSON(
+		'histories', array( '*' ), (
+			$_REQUEST['id'] != 'undefined' ? array( ' id = '.$_REQUEST['id'].' ' ) : ''
+		)
+	);
+
+	if( is_array( $return ) ){
+		echo '{ code: ', $return['code'], ', message: "',sanitize( $return['message'] ),(
+			$return['query'] ? '", query: "'.$return['query'] : ''
+		),'" }';
+	} else {
+		echo $return;
+	}
+}
+
+// TASKS
+
+function addTask(){
+	$return = insert(
+		'tasks', array( 'idHistory','idStatus','name','text' ),
+		array( $_REQUEST['history'],$_REQUEST['status'],$_REQUEST['name'],$_REQUEST['text'] )
+	);
+
+	echo '{ code: ', $return['code'] ,', id: ', $return['id'] ,', message: "',$return['message'],(
+			$return['query'] ? '", query: "'.$return['query'] : ''
+		),'" }';
+}
+
+function getTask(){
+	$return = getJSON(
+		'tasks', array( '*' ), array( 'id = '.$_REQUEST['id'] )
+	);
+
+	if( is_array( $return ) ){
+		echo '{ code: ', $return['code'], ', message: "',sanitize( $return['message'] ),(
+			$return['query'] ? '", query: "'.$return['query'] : ''
+		),'" }';
+	} else {
+		echo $return;
+	}
+}
+
+function getTaskByHistory(){
+	$return = getJSON(
+		'tasks', array( '*' ), array( 'idHistory = '.$_REQUEST['id'] )
+	);
+
+	if( is_array( $return ) ){
+		echo '{ count: 0, code: ', $return['code'], ', message: "',sanitize( $return['message'] ),(
+			$return['query'] ? '", query: "'.$return['query'] : ''
+		),'" }';
+	} else {
+		echo $return;
+	}
+}
+
+function saveStatus(){
+	$return = update(
+		'tasks', array(
+			'idStatus' => $_REQUEST['status'],
+		), array( 'id = '.$_REQUEST['id'] )
+	);
+
+	echo '{ code: ', $return['code'] ,', message: "',$return['message'],(
+		$return['query'] ? '", query: "'.$return['query'] : ''
+	),'" }';
+}
+
+// OTHER
 
 function blank(){
-	echo utf8_encode( '{ errro: 1, message: "Ação inválida" }' );
-}
-
-var_dump( $error_array );
-
-if( $error_array != null ){
-	var_dump( $error_array );
+	die( '{ code: 1, message: "Invalid action" }' );
 }
 ?>
--- trunk/includes/functions.php
+++ trunk/includes/functions.php
@@ -1,15 +1,15 @@
 <?php
+//error_reporting(0);
 require('config.php');
 
 function connect(){
 	$conn = mysql_connect( DB_HOST,DB_USER,DB_PASS );
-	if( is_resource( $conn) ){
+	if( is_resource( $conn ) ){
 		$db = mysql_select_db( DB_DATABASE,$conn );
-		if( is_resource( $db ) ){
-			echo 'connected :)';
-		} else {
+		if( !is_resource( $db ) ){
 			return printError();
 		}
+		return $conn;
 	} else {
 		printError();
 	}
@@ -33,4 +33,115 @@ function printError( $return = false, $message = false ){
 	}
 }
 
+function sanitize( $buf ){
+	return str_replace( '"',"'", str_replace( "\n",'',$buf ) );
+}
+
+function insert( $table = false, $fields = false, $values = false ){
+	if( !$table || !is_array( $fields ) || !is_array( $values ) ){
+		return array(
+			'status' => false, 'code' => 1, 'message' => 'Parâmetros inválidos'
+		);
+	}
+
+	$sql = 'INSERT INTO '.
+				$table.
+					' ('. implode( ',',$fields ) .') '.
+				' VALUES '.
+					' ("'. implode( '","',$values ) .'")';
+
+	$query = mysql_query( $sql );
+
+	if( $query && mysql_affected_rows() > 0 ){
+		return array(
+			'status' => true, 'code' => mysql_errno(), 'id' => mysql_insert_id(),
+			'message' => sanitize( mysql_error() ), 'query' => sanitize( $sql )
+		);
+	} else {
+		return array(
+			'status' => false, 'code' => mysql_errno(), 'id' => 0,
+			'message' => sanitize( mysql_error() ), 'query' => $sql
+		);
+	}
+}
+
+function select( $table = false, $fields = false, $where = false ){
+	if( !$table || !is_array( $fields ) ){
+		return array(
+			'status' => false, 'code' => 1, 'message' => 'Parâmetros inválidos'
+		);
+	}
+
+	$sql = ' SELECT '.
+				implode( ', ',$fields ) .
+			' FROM '.
+				$table.
+			( is_array( $where ) && count( $where ) ?
+				' WHERE '.implode( ' AND ',$where ) : '' );
+
+	$result = mysql_query( $sql );
+
+	if( $result && mysql_num_rows( $result ) > 0 ){
+		$data = array();
+
+		while( $buf = mysql_fetch_assoc( $result ) ){
+			$data[] = $buf;
+		}
+
+		return array(
+			'status' => true, 'code' => mysql_errno(), 'count' => count( $data ), 'data' => $data
+		);
+	} else {
+		return array(
+			'status' => false, 'code' => mysql_errno(), 'message' => sanitize( mysql_error() ),
+			'query' => $sql, 'count' => 0, 'data' => array()
+		);
+	}
+}
+
+function update( $table = false, $values = false, $where = false ){
+	if( !$table || !is_array( $values ) ){
+		return array(
+			'status' => false, 'code' => 1, 'message' => 'Parâmetros inválidos'
+		);
+	}
+
+	$sql = 'UPDATE '.$table.' SET ';
+
+	foreach( $values as $field => $value ){
+		$sql .= ( $i++ ? ',' : '' ).$field.' = "'. $value .'"';
+	}
+	
+	$sql .= $where && is_array( $where )
+		? ' WHERE '. implode( ' AND ',$where ) : '';
+
+	$query = mysql_query( $sql );
+
+	return array(
+		'status' => ( $query && mysql_affected_rows() > 0 ), 'code' => mysql_errno(),
+		'message' => sanitize( mysql_error() ), 'query' => sanitize( $sql )
+	);
+}
+
+function getJSON( $table = false, $fields = false, $where = false ){
+	$return = select( $table, $fields, $where );
+
+	if( $return['status'] ){
+		$buf = '{ count: '. $return['count'].', data: [';
+			$i = 0;
+			foreach( $return['data'] as $row ){
+				$buf .= ( $i++ ? ',' : '' ).'{';
+				$j = 0;
+				foreach( $row as $key => $value ){
+					$buf .= ( $j++ ? ',' : '' ).$key.': "'. sanitize( $value ) .'"';
+				}
+				$buf .= '}';
+			}
+		$buf .= ']}';
+
+		return $buf;
+	} else {
+		return $return;
+	}
+}
 ?>
